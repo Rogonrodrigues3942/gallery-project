93f6bb6461b497f800dcf29983b164f5ba9ffe39
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-12 11:09:32 -0300

    Updatng trn-sql-alter-header.sql, altering header.

--- trn-sql-alter-header.sql
+++ trn-sql-alter-header.sql
@@ -7,3 +7,13 @@ select 	codigo_vendedor	numero,
 		nome_vendedor 	vendedor,
 		salario_fixo 	fixo
 from vendedor
+
+
+--C.2) Exemplo 2
+
+select	numero_pedido "Pedido nº",
+		codigo_cliente "Código do Cliente",
+		prazo_entrega "Prazo de Entrega"
+from pedido
+
+
